dc91f5e3b31ed8bc009d1e01f8283678c3fc0583
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-11 16:51:48 +0200

    Creates an indexed table view

---
+++ IndexedTableDemo/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  IndexedTableDemo
+//
+//  Created by Simon Ng on 3/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
