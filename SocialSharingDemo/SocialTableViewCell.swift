3f00fe525d4b1ce12a523b2684fe19ae6d25e1f7
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-12 14:38:24 +0200

    Adds possibility to share restaurant photos on Facebook

---
+++ SocialSharingDemo/SocialTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SocialTableViewCell.swift
+//  SocialSharingDemo
+//
+//  Created by Simon Ng on 5/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class SocialTableViewCell: UITableViewCell {
+
+    @IBOutlet var featuredImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var shareButton: UIButton!
+}
