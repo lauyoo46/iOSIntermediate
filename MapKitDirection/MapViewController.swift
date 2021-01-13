450bb1973f91536a3fcf6ca6df44bcd29221e5c3
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-13 13:25:39 +0200

    Adds directions & route on Map to the selected restaurant

---
+++ MapKitDirection/MapViewController.swift
@@ -0,0 +1,191 @@
+//
+//  MapViewController.swift
+//  MapKitDirection
+//
+//  Created by Simon Ng on 6/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
+    var currentRoute: MKRoute?
+    var restaurant: Restaurant?
+    let locationManager = CLLocationManager()
+    var currentPlacemark: CLPlacemark?
+    var currentTransportType = MKDirectionsTransportType.automobile
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        segmentedControl.isHidden = true
+        
+        segmentedControl.addTarget(self, action: #selector(showDirection), for: .valueChanged)
+        
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+        
+        let geoCoder = CLGeocoder()
+        guard let safeRestaurant = restaurant else {
+            return
+        }
+        geoCoder.geocodeAddressString(safeRestaurant.location, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                let placemark = placemarks[0]
+                self.currentPlacemark = placemark
+                
+                let annotation = MKPointAnnotation()
+                annotation.title = safeRestaurant.name
+                annotation.subtitle = safeRestaurant.type
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+        
+        mapView.delegate = self
+        if #available(iOS 9.0, *) {
+            mapView.showsCompass = true
+            mapView.showsScale = true
+            mapView.showsTraffic = true
+        }
+    }
+    
+    @objc @IBAction func showDirection(sender: UIButton) {
+        
+        switch segmentedControl.selectedSegmentIndex {
+        
+            case 0: currentTransportType = .automobile
+                
+            case 1: currentTransportType = .walking
+                
+            default: break
+        }
+        
+        segmentedControl.isHidden = false
+        
+        guard let currentPlacemark = currentPlacemark else {
+            return
+        }
+        
+        let directionRequest = MKDirections.Request()
+        
+        directionRequest.source = MKMapItem.forCurrentLocation()
+        let destinationPlacemark = MKPlacemark(placemark: currentPlacemark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlacemark)
+        directionRequest.transportType = currentTransportType
+        
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculate { (routeResponse, routeError) in
+            
+            guard let routeResponse = routeResponse else {
+                if let routeError = routeError {
+                    print("Error: \(routeError)")
+                }
+                return
+            }
+            let route = routeResponse.routes[0]
+            self.currentRoute = route
+            self.mapView.removeOverlays(self.mapView.overlays)
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showSteps" {
+            let routeTableViewController = segue.destination.children[0] as? RouteTableViewController
+            
+            if let routeTableVC = routeTableViewController,
+               let steps = currentRoute?.steps {
+                routeTableVC.routeSteps = steps
+            }
+        }
+    }
+}
+
+// MARK: - MKMapViewDelegate methods
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        var annotationView: MKAnnotationView?
+        
+        if #available(iOS 11.0, *) {
+            var markerAnnotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+            
+            if markerAnnotationView == nil {
+                markerAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                markerAnnotationView?.canShowCallout = true
+            }
+            
+            markerAnnotationView?.glyphText = "😋"
+            markerAnnotationView?.markerTintColor = UIColor.orange
+            
+            annotationView = markerAnnotationView
+            
+        } else {
+            var pinAnnotationView: MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+            
+            if pinAnnotationView == nil {
+                pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                pinAnnotationView?.canShowCallout = true
+                pinAnnotationView?.pinTintColor = UIColor.orange
+            }
+            
+            annotationView = pinAnnotationView
+        }
+        
+        annotationView?.rightCalloutAccessoryView = UIButton(type: UIButton.ButtonType.detailDisclosure)
+        
+        guard let safeRestaurant = restaurant else {
+            return annotationView
+        }
+        
+        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+        leftIconView.image = UIImage(named: safeRestaurant.image)
+        annotationView?.leftCalloutAccessoryView = leftIconView
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 3.0
+        
+        return renderer
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        performSegue(withIdentifier: "showSteps", sender: view)
+    }
+    
+}
