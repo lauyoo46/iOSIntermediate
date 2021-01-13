c2cb54c57fcc016e061c214f39c6c26a911b6a70
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-13 15:49:00 +0200

    Adds possibility to search for nearby points of interest

---
+++ MapKitDirection/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  MapKitDirection
+//
+//  Created by Simon Ng on 6/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
