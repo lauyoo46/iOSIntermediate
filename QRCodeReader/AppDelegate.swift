17ddfefde0e34bf22d788b1c53fe822560e78108
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-13 17:46:02 +0200

    Demo app for scaning QR Codes

---
+++ QRCodeReader/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  QRCodeReader
+//
+//  Created by Simon Ng on 13/10/2019.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+}
+
