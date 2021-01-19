15319ac8885d43cfcbe79e13e08941d7abb0051d
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-19 18:05:43 +0200

    Adapts content of a collection view to fit all iphones in portrait mode using size classes

---
+++ DoodleFun/DoodleCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DoodleCollectionViewCell.swift
+//  DoodleFun
+//
+//  Created by Laurentiu Ile on 19/01/2021.
+//  Copyright © 2021 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class DoodleCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
