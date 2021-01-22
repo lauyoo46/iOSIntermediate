3905b377ffbdc0f137b84523b648333ae910e751
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-22 09:31:15 +0200

    Adds Slide down side menu

---
+++ SlideDownMenu/NewsTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsTableViewCell.swift
+//  SlideDownMenu
+//
+//  Created by Simon Ng on 27/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet var postImageView:UIImageView!
+    @IBOutlet var postTitle:UILabel!
+    @IBOutlet var postAuthor:UILabel!
+    @IBOutlet var authorImageView:UIImageView!
+    
+}
