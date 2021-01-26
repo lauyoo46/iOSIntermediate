bff49aa27ea03834ffd6789d7ecb22bf9c60121a
laurentiuile <ilelaurentiu@yahoo.com>
2021-01-26 13:06:36 +0200

    Preloads data from a csv file at application launch into the SQLite database

---
+++ CoreDataPreloadDemo/MenuTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTableViewCell.swift
+//  CoreDataPreloadDemo
+//
+//  Created by Simon Ng on 7/11/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    
+}
