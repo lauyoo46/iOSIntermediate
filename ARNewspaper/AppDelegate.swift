3124fa88994ada49a8b194c638a6768cc3f54919
laurentiuile <ilelaurentiu@yahoo.com>
2021-02-04 16:38:36 +0200

    Creates app wich plays snowboarding video in a newspaper

---
+++ ARNewspaper/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  ARNewspaper
+//
+//  Created by Laurentiu Ile on 04/02/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
